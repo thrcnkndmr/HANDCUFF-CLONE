b179b8dd29c45d6973a5497f5c09cbf61e9d5fa9
Tahircan Kandemir <68933000+thrcnkndmr@users.noreply.github.com>
2022-10-26 22:28:32 +0300

    +

---
+++ Assets/Scripts/HandcuffCollect.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandcuffCollect : MonoBehaviour
+{
+    public List<GameObject> handcuffList = new List<GameObject>();
+    public GameObject handcuffPrefab;
+    public Transform collectPoint;
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,28 +9,39 @@ public class PlayerController : MonoBehaviour
     public DynamicJoystick dynamicJoystick;
     public float speed;
     public float turnSpeed;
+
+    private Animator playerAnimator;
     
     private TouchManager touchManager;
     
     private void Start()
     {
         touchManager = TouchManager.Instance;
+        playerAnimator = GetComponent<Animator>();
     }
 
     public void OnEnable()
     {
         touchManager = TouchManager.Instance;
         touchManager.onTouchMoved += OnTouchMoved;
+        touchManager.onTouchEnded += OnTouchEnded;
+
     }
 
     public void OnDisable()
     {
         touchManager = TouchManager.Instance;
         touchManager.onTouchMoved -= OnTouchMoved;
+        touchManager.onTouchEnded -= OnTouchEnded;
+
+        
     }
 
     private void OnTouchMoved(TouchInput touchInput)
     {
+        playerAnimator.SetTrigger("Run");
+
+
         float horizontal = dynamicJoystick.Horizontal;
         float vertical = dynamicJoystick.Vertical;
         Vector3 addedPos = new Vector3(horizontal * speed * Time.deltaTime, 0, vertical * speed * Time.deltaTime);
@@ -39,4 +50,10 @@ public class PlayerController : MonoBehaviour
         Vector3 direction = Vector3.forward * vertical + Vector3.right * horizontal;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction),turnSpeed*Time.deltaTime);
     }
+
+    private void OnTouchEnded(TouchInput touchInput)
+    {
+        playerAnimator.SetTrigger("Idle");
+    }
+    
 }
