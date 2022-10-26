afc2bea875b4af355662a73b1226a689c7843925
Tahircan Kandemir <68933000+thrcnkndmr@users.noreply.github.com>
2022-10-26 21:21:46 +0300

    cinemachine eklendi & kontroller eklendi

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,42 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Blended;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public DynamicJoystick dynamicJoystick;
+    public float speed;
+    public float turnSpeed;
+    
+    private TouchManager touchManager;
+    
+    private void Start()
     {
-        
+        touchManager = TouchManager.Instance;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnEnable()
     {
+        touchManager = TouchManager.Instance;
+        touchManager.onTouchMoved += OnTouchMoved;
+    }
+
+    public void OnDisable()
+    {
+        touchManager = TouchManager.Instance;
+        touchManager.onTouchMoved -= OnTouchMoved;
+    }
+
+    private void OnTouchMoved(TouchInput touchInput)
+    {
+        float horizontal = dynamicJoystick.Horizontal;
+        float vertical = dynamicJoystick.Vertical;
+        Vector3 addedPos = new Vector3(horizontal * speed * Time.deltaTime, 0, vertical * speed * Time.deltaTime);
+        transform.position += addedPos;
         
+        Vector3 direction = Vector3.forward * vertical + Vector3.right * horizontal;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction),turnSpeed*Time.deltaTime);
     }
 }
